3b39472a153b03579a7df3cedbada8460ed8e451
MariamKipiani <145837678+MariamKipiani@users.noreply.github.com>
2023-12-25 01:59:41 +0400

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by user on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
